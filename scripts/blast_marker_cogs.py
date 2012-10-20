8c370209b101893c4c755add5107c11d4e375ada
Javier Velazquez-Muriel <javi.velazquez@gmail.com>
2012-10-19 20:04:59 -0700

    Bast via subprocess is working

--- scripts/blast_marker_cogs.py
+++ scripts/blast_marker_cogs.py
@@ -1,11 +1,99 @@
 
 
 import Binner.MetagenomeDatabase as MetagenomeDatabase
+from Bio.Blast.Applications import NcbipsiblastCommandline
+from Bio.Blast.Applications import NcbiblastpCommandline
+import multiprocessing
+import subprocess
 import sys
 import os
 import time
 import logging
 log = logging.getLogger("blast_marker_cogs")
+
+
+def do_blast(sequence, identifier ):
+    """
+        Do a BLAST run
+        @param sequence A protein sequence
+        @param identifer A identifier to name temporary input file and the
+                output file
+    """
+    if len(sequence) == 0:
+        raise ValueError("Empty sequence")
+    fn = "{0}.fasta".format(identifier)
+    fn_output = "{0}.xml".format(identifier)
+    fhandle = open(fn, "w")
+    fhandle.write(">" + identifier + "\n" + sequence + "\n")
+    fhandle.close()
+    p = NcbiblastpCommandline(query=fn, db="nr", evalue=0.001,outfmt=5, out=fn_output)
+    command = str(p)
+    log.info("running BLAST: %s", command)
+    # Popen does not like a string, I have to split in the whitespaces
+    process_id = subprocess.Popen(command.split(" "),shell=False,env=os.environ,
+                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    (out, err) = process_id.communicate()
+    if (err != ""): # there is some error output
+        log.error( "There was an error: %s" % err)
+        raise IOError
+    os.remove(fn)
+
+
+def do_multiprocessing_blast(sequences, identifiers):
+    """
+        Do a BLAST run using various processors
+        @param sequences A set of protein sequences
+        @param identifer A set of identifiers
+    """
+    if len(sequences) == 0:
+        raise ValueError("")
+    fn = "{0}.fasta".format(identifier)
+    fn_output = "{0}.xml".format(identifier)
+    fhandle = open(fn, "w")
+    fhandle.write(">" + identifier + "\n" + sequence + "\n")
+    fhandle.close()
+    p = NcbipsiblastCommandline(query=fn, db="/chime1/home/javi/bioinformatics/ncbi_databases/nr", evalue=0.001,
+                                     outfmt=5, out=fn_output)
+    psiblast_command = str(p)
+    log.info("running BLAST: %s", psiblast_command)
+    # Popen does not like a string, I have to split in the whitespaces
+    process_id = subprocess.Popen(psiblast_command.split(" "),shell=False,env=os.environ,
+                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    (out, err) = process_id.communicate()
+    if (err != ""): # there is some error output
+        log.error( "There was an error: %s" % err)
+        raise IOError
+    os.remove(fn)
+
+
+def (sequence, identifier ):
+    """
+        Do a BLAST run
+        @param sequence A protein sequence
+        @param identifer A identifier to name temporary input file and the
+                output file
+    """
+    if len(sequence) == 0:
+        raise ValueError("Empty sequence")
+    fn = "{0}.fasta".format(identifier)
+    fn_output = "{0}.xml".format(identifier)
+    fhandle = open(fn, "w")
+    fhandle.write(">" + identifier + "\n" + sequence + "\n")
+    fhandle.close()
+    p = NcbipsiblastCommandline(query=fn, db="/chime1/home/javi/bioinformatics/ncbi_databases/nr", evalue=0.001,
+                                     outfmt=5, out=fn_output)
+    psiblast_command = str(p)
+    log.info("running BLAST: %s", psiblast_command)
+    # Popen does not like a string, I have to split in the whitespaces
+    process_id = subprocess.Popen(psiblast_command.split(" "),shell=False,env=os.environ,
+                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    (out, err) = process_id.communicate()
+    if (err != ""): # there is some error output
+        log.error( "There was an error: %s" % err)
+        raise IOError
+    os.remove(fn)
+
+
 
 def blast_marker_cogs(args):
     log.debug("Running blast for the marker COGS")
@@ -38,10 +126,15 @@ def blast_marker_cogs(args):
                               AND {0}.gene_id={1}.gene_id """.format(
                                 db.GenesTable,db.SequenceTable,cog)
         log.debug("%s",sql_command)
-        seqs = db.retrieve_data(sql_command)
+        records = db.retrieve_data(sql_command)
+        if not len(genes) == len(records):
+            raise ValueError,"The number of genes recovered is not the same as " \
+                "the number of sequences"
+        for record in records[0:3]:
+            sequence = record[2]
+            gene_id = record[0]
+            do_blast(sequence,gene_id)
     db.close()
-    for row in seqs:
-        print row[0],row[1],len(row[2])
 
 
 if __name__ == "__main__":
