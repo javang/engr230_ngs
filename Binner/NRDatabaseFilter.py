f7985011152c13afa05bed1db44ceda2b82d5e29
Javier Velazquez-Muriel <javi.velazquez@gmail.com>
2012-10-22 00:51:23 -0700

    Forgot to commit more files related to the database filtering script

---
+++ Binner/NRDatabaseFilter.py
@@ -0,0 +1,132 @@
+import Bio.SeqIO as SeqIO
+import sys
+import os
+import time
+import logging
+import csv
+import collections
+
+log = logging.getLogger("NRDatabaseFilter")
+
+def remove_common_words(words):
+    """ Remove common words for the list of words given"""
+    common_words = ["the", "for", "of" ]
+    return [w for w in words if w not in common_words ]
+
+def contains_all_keywords(words, keywords):
+    """
+        Returns true in all the words are present in the keywords.
+        @param words The words to test.
+        @param keywords The keywords that the words must contain
+        @return Returns true if the words contain all the keywords
+    """
+    for keyword in keywords:
+        if not keyword in words:
+            return False
+    return True
+
+
+class NRDatabaseFilter:
+
+    def __init__(self, fn_nr_database):
+        """
+            file of the NR database
+        """
+        self.nr = fn_nr_database
+        self.ids = []
+
+    def set_ids(self, ids):
+        """
+            Sets a set of ids for the descriptions. The are used to build filenames.
+            If they are not provided, the files with be called database.nrXXX, where
+            XXX are numeric characters.
+            @param ids
+        """
+        log.debug("Ids for the descriptions set: %s", ids)
+        self.ids = ids
+
+    def set_descriptions(self, descriptions):
+        """
+            @param set of descriptions used to filter sequences. The descriptions are
+            split in keywords and common words are removed from the keywords
+
+        """
+        log.debug("Setting descriptions to filter")
+        if len(descriptions) == 0:
+            raise ValueError("The list of descriptions is empty")
+        self.descriptions_keywords = []
+        for d in descriptions:
+            keywords = d.lower().split(" ")
+            self.descriptions_keywords.append(keywords)
+
+    def do_filtering(self,overwrite=True):
+        """
+            Does the filtering seleting the sequences with descriptions matching
+            the descriptions stored in the class
+            @param overwrite
+        """
+        log.info("Filtering database with the descriptions provided")
+        if len(self.descriptions_keywords) == 0:
+           raise ValueError("The database cannot be filtered. No descriptions " \
+           "to extract are given")
+        if len(self.ids) == 0 and len(self.ids) != len(self.descriptions_keywords):
+            raise ValueError("The number of ids provided do not match the number " \
+                "of descriptions. An id per description is needed. ")
+        if overwrite:
+            map(os.remove, self.get_database_files_created())
+        for seq_record in SeqIO.parse(self.nr, "fasta"):
+            found, ind = self.compare_to_descriptions(seq_record.description)
+
+            # if the description for the sequence is found in the descriptions
+            # to extract, write to its database file
+            n_ids = len(self.ids)
+            if found:
+                log.debug("Found match for %s" % seq_record.description)
+                if not n_ids:
+                    fn = "nr.filtered.{0:06d}".format(ind)
+                else:
+                    fn = self.get_database_name(self.ids[ind])
+                    log.debug("Appending %s (index %d) to %s",self.ids[ind],ind,fn)
+                f = open(fn, "a")
+                f.write(seq_record.format("fasta"))
+                f.close()
+
+    def compare_to_descriptions(self, description):
+        """ Compare a description to all the descriptions that are going to
+            be filtered. If the description matches any of the descriptions
+                             stored in the class, returns True.
+            @param description The description to compare
+            @return A bool value and an index. The bool is true if the
+            description is found within the descriptions stored in the class.
+            The index is the index of the description that matches the
+            argument
+        """
+#        log.debug("Checking description %s",description)
+        words = description.lower().split(" ")
+        for i, keywords in enumerate(self.descriptions_keywords):
+#            log.debug("Checking keywords: %s",keywords)
+            if contains_all_keywords(words, keywords):
+                return True, i
+        return False, 0
+
+    def get_database_files_created(self):
+        """
+            returns the names of all the files created by the function
+            do_filtering()
+        """
+        if len(self.ids) == 0:
+            return glog.glob("nr.filtered*")
+        else:
+            fns = []
+            for i in self.ids:
+                fn = self.get_database_name(i)
+                if os.path.exists(fn):
+                    fns.append(fn)
+            return fns
+
+    def get_database_name(self,identifier):
+        """
+            @param identifier A string
+            @returns A name for a database file based on the identifier
+        """
+        return "nr." + identifier
---
+++ test/test_filter_nr_database.py
@@ -0,0 +1,78 @@
+
+import unittest
+import os
+import csv
+import Binner.NRDatabaseFilter as NRDatabaseFilter
+import Bio.SeqIO as SeqIO
+import sys
+import utility
+import logging
+log = logging.getLogger("TestNRDatabaseFilter")
+
+class TestNRDatabaseFilter(unittest.TestCase):
+
+    def setUp(self):
+        """
+            Prepare the test file
+        """
+        self.datadir = utility.get_data_directory(__file__)
+
+    def test_remove_common_words(self):
+        """ Test removing common words """
+        d = "This is the sentence of words to process"
+        words = NRDatabaseFilter.remove_common_words(d.split())
+        self.assertTrue(not "the" in words)
+        self.assertTrue(not "of" in words)
+        self.assertTrue("sentence" in words)
+        self.assertTrue("This" in words)
+
+    def test_contains_all_keywords(self):
+        """ Test contains all keywords """
+        words = "tRNA amylase somethig protein".split()
+        keywords = "amylase protein".split()
+        self.assertTrue(NRDatabaseFilter.contains_all_keywords(words,keywords))
+
+
+    def test_filtering(self):
+        """ test the filtering of the NR database """
+
+        f = open(os.path.join(self.datadir,"coglist13962_10-oct-2012.txt"),"rU") # rU read with universal new line
+        reader = csv.reader(f, delimiter="\t")
+        reader.next() # discard title line
+        i = 0
+        cogs_names = []
+        cogs_ids = []
+        for row in reader:
+            cogs_ids.append(row[0])
+            cogs_names.append(row[1].lower())
+            i += 1
+            if i > 5:
+                break
+        f.close()
+        log.debug("Filtering COGS %s",cogs_ids)
+        log.debug("Descriptions: %s",cogs_names)
+        fn_database = os.path.join(self.datadir, "nr_test")
+        dbfilter = NRDatabaseFilter.NRDatabaseFilter(fn_database)
+        dbfilter.set_ids(cogs_ids)
+        dbfilter.set_descriptions(cogs_names)
+        dbfilter.do_filtering(overwrite=True)
+
+        for cog_id, name in zip(cogs_ids, cogs_names):
+            fn = dbfilter.get_database_name(cog_id)
+            if os.path.exists(fn):
+                # check that the file contains only sequences that
+                # match the description
+                keywords = name.lower().split()
+                for seq_record in SeqIO.parse(fn, "fasta"):
+                    words = seq_record.description.lower().split(" ")
+                    self.assertTrue(
+                        NRDatabaseFilter.contains_all_keywords(words,keywords),
+                        "There is a sequence in file {0} that does not have " \
+                        "the right kewords {1}".format(fn,str(keywords)))
+        map(os.remove, dbfilter.get_database_files_created())
+
+if __name__ == "__main__":
+    logging.basicConfig(stream=sys.stdout)
+    logging.root.setLevel(logging.INFO)
+    unittest.main()
+
