f4214c6a5f8e4351abb7c33570a9f870aa1f578d
Javier Velazquez-Muriel <javi.velazquez@gmail.com>
2012-10-29 16:56:36 -0700

    Renamed the script for the marker cogs.

--- scripts/blast_marker_cogs.py
+++ scripts/blast_marker_cogs.py
@@ -1,21 +1,39 @@
 
-
 import Binner.MetagenomeDatabase as MetagenomeDatabase
 import Binner.MultiProcessingBLAST as MultiProcessingBLAST
 
-from Bio.Blast.Applications import NcbipsiblastCommandline
-from Bio.Blast.Applications import NcbiblastpCommandline
-import multiprocessing
-import subprocess
 import sys
 import os
 import time
+import csv
 import logging
 log = logging.getLogger("blast_marker_cogs")
 
+def blast(seqs):
+    """
+        Blast a set of sequences and parse the results. The function does calls the
+        MultiProcessing versions
+        @seqs A list of tuples of (sequence, identifier for the sequence, database to use
+        for the blast procedure)
+    """
+    if len(seqs) == 0:
+       raise ValueError("No sequences provided")
+    blaster = MultiProcessingBLAST.MultiProcessingBLAST()
+    log.debug("Running blast from %s sequences",len(seqs))
+    for seq in seqs:
+        blaster.add_sequence(*seq)
+    fns_blast_output = blaster.run()
+    parser = MultiProcessingBLAST.MultiProcessingBLASTParser()
+    for identifier, fn in fns_blast_output:
+        parser.add_file(identifier, fn)
+    parsing_results = parser.run()
+    # clean xmls after parsing
+    for i,fn in fns_blast_output:
+        os.remove(fn)
+    return parsing_results
+
 
 def blast_marker_cogs(args):
-    log.info("Running blast for the marker COGS")
     db = MetagenomeDatabase.MetagenomeDatabase()
     db.connect(args.fn_database)
     names = db.get_tables_names()
@@ -25,57 +43,81 @@ def blast_marker_cogs(args):
         raise ValueError("The database does not have a table of genes")
     if not db.SequenceTable in names:
         raise ValueError("The database does not have a table sequences")
-    # get marker cogs
-    sql_command = """SELECT cog_id FROM {0}""".format(db.MarkersTable)
-    markers = db.retrieve_data(sql_command)
-    if len(markers) == 0:
-        raise ValueError("There are no marker COGs in the database")
 
-    blaster = MultiProcessingBLAST.MultiProcessingBLAST()
+    # Read file marker cogs
+    fhandle = open(args.fn_marker_cogs, "r")
+    reader = csv.reader(fhandle, delimiter="\t")
+    markercogs = frozenset([row[0] for row in reader])
+    if len(markercogs) == 0:
+        raise ValueError("No marker COGs provided")
 
-    for m in markers[0:1]:
-        cog = m[0]
-        log.debug("Marker COG %s",cog)
-        sql_command = """SELECT * FROM {0}
-                        WHERE cog_id="{1}" """.format(
-                                db.GenesTable,cog)
-        log.debug("%s",sql_command)
-        genes = db.retrieve_data(sql_command)
-        # select genes that have the marker COG annotation
-        sql_command = """SELECT {0}.gene_id,{0}.protein_length,sequence FROM {0}
-                        INNER JOIN {1}
-                        WHERE cog_id="{2}"
-                              AND {0}.gene_id={1}.gene_id """.format(
-                                db.GenesTable,db.SequenceTable,cog)
-        log.debug("%s",sql_command)
-        records = db.retrieve_data(sql_command)
-        if not len(genes) == len(records):
-            raise ValueError,"The number of genes recovered is not the same as " \
-                "the number of sequences"
-        for record in records[0:20]:
-            sequence = record[2]
-            gene_id = record[0]
-            blaster.add_sequence(sequence, gene_id)
-#            do_blast(sequence,gene_id)
-        
-    blaster.run()
+    for cog in markercogs:
+        fn = os.path.join(args.cogsdbdir, cog + ".phr")
+        if not os.path.exists(fn):
+            raise IOError(
+            "The database file {0} for the COG {1} does not exist".format(fn,cog))
+
+    # Get genes
+    sql_command = """SELECT gene_id,cog_id FROM {0}""".format(db.GenesTable)
+    data = db.retrieve_data(sql_command)
+    if db.BlastResultsTable in names:
+        db.drop_table(db.BlastResultsTable)
+    db.create_blast_results_table()
     db.close()
+
+    log.info("Running BLAST for %s marker COGS",len(markercogs))
+    n_batch_sequences = 100 # sequences to blast per batch
+    sequence_tuples = []
+    for gene_id,cog_id in data:
+        if cog_id in markercogs:
+            db.connect(args.fn_database)
+            sql_command = """SELECT sequence FROM {0}
+                        WHERE gene_id="{1}" """.format(db.SequenceTable, gene_id)
+            records = db.retrieve_data(sql_command)
+            db.close()
+            if len(records) != 1:
+                # Report but do not raise, continue processing other genes
+                log.error("Problem with gene_id %s. There are no sequences is the database or "
+                "there are more than one", gene_id)
+                continue
+            blast_database = os.path.join(args.cogsdbdir, cog_id)
+            sequence_tuples.append((records[0][0], gene_id, blast_database))
+            if len(sequence_tuples) == n_batch_sequences:
+                batch_results = blast(sequence_tuples)
+                db.connect(args.fn_database)
+                db.store_blast_results(batch_results)
+                db.close()
+                sequence_tuples = []
+    # Final run
+    if len(sequence_tuples):
+        batch_results = blast(sequence_tuples)
+        batch_genes_ids = [tup[1] for tup in sequence_tuples]
+        db.connect(args.fn_database)
+        db.store_blast_results(batch_results)
+        db.close()
 
 
 if __name__ == "__main__":
 
     import argparse
     parser = argparse.ArgumentParser(
-        description="Creates a database of COGs and sequences from a IMG/M " \
-        "metagenome")
+        description="BLAST all the proteins in the metagenome against a set of marker COGs")
 
     parser.add_argument("fn_database",
-                    help="File with gene descriptions as provided by IMG/M")
+                    help="Datbabase formed by the files provided by the IMG/M for a metagenome. " \
+                    "This database needs to be created with teh create_database.py script")
+    parser.add_argument("fn_marker_cogs",
+                    help="File of COG ids that are going to be used as markers. The protein sequences of the " \
+                    "metagenome that are predicted to be part of a COG are aligned against all the sequences " \
+                    "with that COG in the NR database")
+    parser.add_argument("--cogsdbdir",
+                        default=".",
+                        help="Directory where to locate the BLAST databases of the COGs. These " \
+                        "databases nweed to be generated using makeblastdb from BLAST.")
     parser.add_argument("--log",
                     dest="log",
                     default = False,
                     help="Log file")
-
     args = parser.parse_args()
     if(args.log):
         logging.basicConfig(filename=args.log, filemode="w")
