9f3b2b17a27179aee80551ec3d7c331bd853cd1f
Javier Velazquez-Muriel <javi.velazquez@gmail.com>
2012-11-01 13:10:38 -0700

    Added the file to store rules of the program that have to do with biology

--- Binner/BiologyBasedRules.py
+++ Binner/BiologyBasedRules.py
@@ -9,8 +9,22 @@ import collections
 log = logging.getLogger("BiologyBasedRules")
 
 
-def select_genus(organisms):
+def select_genus_by_majority(organisms):
+    """ Selects the most frequently appearing genus in the organisms
+
+        @param organisms An iterable of scientific names
+
+    """
     genuses = [name.split(" ")[0] for name in organisms]
     counter = collections.Counter(genuses)
     g = counter.most_common(1)
     return g[0][0]
+
+
+def select_genus_by_best_hit(organisms):
+    """ Selects the firts appearing genus in the organisms
+
+        @param organisms An iterable of scientific names
+
+    """
+    return organisms[0].split(" ")[0]
