39ac09d54b9ebc172e586f96a973d443be0f9a29
Javier Velazquez-Muriel <javi.velazquez@gmail.com>
2012-11-01 13:13:10 -0700

    Finished script to assign genus to scaffolds.py

--- scripts/assign_genus_to_scaffolds.py
+++ scripts/assign_genus_to_scaffolds.py
@@ -1,119 +1,79 @@
 
 import Binner.MetagenomeDatabase as MetagenomeDatabase
 import Binner.BLASTUtilities as BLASTUtilities
-
+import Binner.BiologyBasedRules as BiologyBasedRules
 import sys
 import os
 import time
 import csv
 import logging
-log = logging.getLogger("blast_marker_cogs")
+log = logging.getLogger("assign genus to scaffolds")
 
-def blast(seqs):
+
+def assign_genus_to_scaffolds(args):
+    """ Assign genus to scaffolds in the database
+
+    The function:
+    1) Reads the genes in the database that belong to a given COG
+    2) Reads the BLAST results for them.
+    3) Assigns the best hit to the scaffold containing the gene
+
     """
-        Blast a set of sequences and parse the results. The function does calls the
-        MultiProcessing versions
-        @seqs A list of tuples of (sequence, identifier for the sequence, database to use
-        for the blast procedure)
-    """
-    if len(seqs) == 0:
-       raise ValueError("No sequences provided")
-    blaster = BLASTUtilities.BLASTMultiProcessing()
-    log.debug("Running blast from %s sequences",len(seqs))
-    for seq in seqs:
-        blaster.add_sequence(*seq)
-    fns_blast_output = blaster.run()
-    parser = BLASTUtilities.BLASTMultiProcessingParser()
-    for identifier, fn in fns_blast_output:
-        parser.add_file(identifier, fn)
-    parsing_results = parser.run()
-    # clean xmls after parsing
-    for i,fn in fns_blast_output:
-        os.remove(fn)
-    return parsing_results
-
-
-def blast_marker_cogs(args):
     db = MetagenomeDatabase.MetagenomeDatabase()
     db.connect(args.fn_database)
     names = db.get_tables_names()
-    if not db.MarkersTable in names:
-        raise ValueError("The database does not have a table of marker COGs")
     if not db.GenesTable in names:
         raise ValueError("The database does not have a table of genes")
-    if not db.SequenceTable in names:
-        raise ValueError("The database does not have a table sequences")
+    if not db.BlastResultsTable in names:
+        raise ValueError("The database does not have a table of BLAST results")
 
     # Read file marker cogs
-    fhandle = open(args.fn_marker_cogs, "r")
-    reader = csv.reader(fhandle, delimiter="\t")
-    markercogs = frozenset([row[0] for row in reader])
-    if len(markercogs) == 0:
+    fhandle = open(args.fn_marker_cogs, "rU")
+    reader = csv.reader(fhandle, delimiter=" ")
+    marker_cogs = frozenset([row[0] for row in reader])
+    if len(marker_cogs) == 0:
         raise ValueError("No marker COGs provided")
 
-    for cog in markercogs:
-        fn = os.path.join(args.cogsdbdir, cog + ".phr")
-        if not os.path.exists(fn):
-            raise IOError(
-            "The database file {0} for the COG {1} does not exist".format(fn,cog))
+    if db.ScaffoldAssignmentsTable in names:
+        db.drop_table(db.ScaffoldAssignmentsTable)
+    db.create_scaffold_assignments_table()
 
-    # Get genes
-    sql_command = """SELECT gene_id,cog_id FROM {0}""".format(db.GenesTable)
-    data = db.retrieve_data(sql_command)
-    if db.BlastResultsTable in names:
-        db.drop_table(db.BlastResultsTable)
-    db.create_blast_results_table()
+    # read the genes and scaffolds for the cog
+    blast_result = BLASTUtilities.BLASTResult()
+    scaffolds_data =[]
+    for cog_id in marker_cogs:
+        sql_command = """SELECT {0}.gene_id,{0}.scaffold,{1}.titles
+                         FROM {0}
+                         INNER JOIN {1}
+                         WHERE {0}.cog_id="{2}" AND {0}.gene_id={1}.gene_id
+                      """.format(db.GenesTable,db.BlastResultsTable,cog_id)
+        data = db.retrieve_data(sql_command)
+        log.info("%s Genes retrieved for %s",len(data),cog_id)
+        # parse organisms
+        for row in data:
+            organism = blast_result.get_best_hit_name(row[2])
+            genus = organism.split(" ")[0]
+            scaffolds_data.append((row[1],genus))
+    db.store_data(db.ScaffoldAssignmentsTable,scaffolds_data)
     db.close()
-
-    log.info("Running BLAST for %s marker COGS",len(markercogs))
-    n_batch_sequences = 100 # sequences to blast per batch
-    sequence_tuples = []
-    for gene_id,cog_id in data:
-        if cog_id in markercogs:
-            db.connect(args.fn_database)
-            sql_command = """SELECT sequence FROM {0}
-                        WHERE gene_id="{1}" """.format(db.SequenceTable, gene_id)
-            records = db.retrieve_data(sql_command)
-            db.close()
-            if len(records) != 1:
-                # Report but do not raise, continue processing other genes
-                log.error("Problem with gene_id %s. There are no sequences is the database or "
-                "there are more than one", gene_id)
-                continue
-            blast_database = os.path.join(args.cogsdbdir, cog_id)
-            sequence_tuples.append((records[0][0], gene_id, blast_database))
-            if len(sequence_tuples) == n_batch_sequences:
-                batch_results = blast(sequence_tuples)
-                db.connect(args.fn_database)
-                db.store_blast_results(batch_results)
-                db.close()
-                sequence_tuples = []
-    # Final run
-    if len(sequence_tuples):
-        batch_results = blast(sequence_tuples)
-        batch_genes_ids = [tup[1] for tup in sequence_tuples]
-        db.connect(args.fn_database)
-        db.store_blast_results(batch_results)
-        db.close()
 
 
 if __name__ == "__main__":
 
     import argparse
     parser = argparse.ArgumentParser(
-        description="BLAST all the proteins in the metagenome against a set of marker COGs")
+        description=""" Assign the genus to the scaffolds of a metagenome based on a set
+                        of marker COGS. Procedure
+                        1) Recover all genes annotated as having a marker COG from the database
+                        2) Recover BLAST results for each of the genes
+                        3) Assign the genus of the microorganism to the scaffold where the gene is
+                    """)
 
     parser.add_argument("fn_database",
                     help="Datbabase formed by the files provided by the IMG/M for a metagenome. " \
                     "This database needs to be created with teh create_database.py script")
     parser.add_argument("fn_marker_cogs",
-                    help="File of COG ids that are going to be used as markers. The protein sequences of the " \
-                    "metagenome that are predicted to be part of a COG are aligned against all the sequences " \
-                    "with that COG in the NR database")
-    parser.add_argument("--cogsdbdir",
-                        default=".",
-                        help="Directory where to locate the BLAST databases of the COGs. These " \
-                        "databases nweed to be generated using makeblastdb from BLAST.")
+                    help="File of COG ids that are going to be used as markers.")
     parser.add_argument("--log",
                     dest="log",
                     default = False,
@@ -123,5 +83,5 @@ if __name__ == "__main__":
         logging.basicConfig(filename=args.log, filemode="w")
     else:
         logging.basicConfig(stream=sys.stdout)
-    logging.root.setLevel(logging.INFO)
-    blast_marker_cogs(args)
+    logging.root.setLevel(logging.DEBUG)
+    assign_genus_to_scaffolds(args)
